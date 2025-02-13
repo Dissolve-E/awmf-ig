81d5298b3ab7a2681f989071f2af1de60c13e0ff
Gregor Lichtner <gregor.lichtner@med.uni-greifswald.de>
2025-02-13 14:53:34 +0100

    ci: add workflows

---
+++ download-ebm-ig.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# this script downloads the CI build of the EBM IG
+
+package_version=$(grep -A1 'hl7.fhir.uv.ebm:' sushi-config.yaml | head -n1 | awk '{ print $2}')
+echo "Detected package version: $package_version"
+
+filename="package.tgz"
+
+# Check if the string was found
+if [ -z "$package_version" ]; then
+    echo "EBM package version not found in sushi-config.yaml"
+    exit 1
+fi
+
+# Ensure the script only runs if the package version is exactly "ci-build"
+if [ "$package_version" != "ci-build" ]; then
+    echo "Package version is not 'ci-build'. Nothing to do."
+    exit 0
+fi
+
+package_url="https://build.fhir.org/ig/HL7/ebm/$filename"
+
+mkdir -p "$HOME/.fhir/packages"
+
+path="$HOME/.fhir/packages/hl7.fhir.uv.ebm#$package_version"
+echo "Downloading to: $path"
+mkdir -p "$path"
+cd "$path" || exit
+
+echo "Downloading $filename..."
+curl -L -o "$filename" -s "$package_url"
+tar -zxf "$filename"
+rm "$filename"
